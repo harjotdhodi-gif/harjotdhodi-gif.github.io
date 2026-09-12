02522023dab80c2e95acedcf527d8d581928f4f3
Scriptorium Lab Services <harjotdhodi@gmail.com>
2026-09-12 07:24:32 +0530

    Add portfolio documentation sidebar

---
+++ sidebars.js
@@ -0,0 +1,11 @@
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  portfolioSidebar: [
+    'experience',
+    'portfolio',
+    'github-projects',
+    'capabilities',
+  ],
+};
+
+module.exports = sidebars;
